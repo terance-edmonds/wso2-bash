2942e35484d985c84a52a45fd66c0611770fbad1
Terance Edmonds <teranceedmonds20@gmail.com>
2024-11-29 12:42:04 +0530

    apk ingress installation

--- apk/install.sh
+++ apk/install.sh
@@ -15,12 +15,6 @@ echo $'helm APIM-APK Agent name: '$helm_agent_name$''
 echo $'\n# Create namespace #'
 kubectl create ns $namespace
 
-echo $'\n# Install Nginx Ingress #'
-BLA::start_loading_animation "${BLA_braille_whitespace[@]}"
-helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace $namespace
-BLA::stop_loading_animation
-
-
 echo $'\n# Add APIM CP, APK DP, APIM-APK Agent repos #'
 helm repo add wso2apim https://github.com/wso2/helm-apim/releases/download/cp-4.4.0
 helm repo add wso2apk https://github.com/wso2/apk/releases/download/1.2.0
@@ -32,6 +26,10 @@ BLA::start_loading_animation "${BLA_braille_whitespace[@]}"
 helm install $helm_apim_name wso2apim/wso2am-cp --version 4.4.0-1 -f https://raw.githubusercontent.com/wso2/apk/main/helm-charts/samples/apim/cp/apk-cp/4.4.0-values.yaml -n $namespace
 BLA::stop_loading_animation
 
+echo $'\n# Install Nginx Ingress #'
+BLA::start_loading_animation "${BLA_braille_whitespace[@]}"
+helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace $namespace
+BLA::stop_loading_animation
 
 echo $'\n# Install APK DP #'
 BLA::start_loading_animation "${BLA_braille_whitespace[@]}"
