43e38c7f18fcf4aefedae1dbd14647fcec3b39f7
Terance Edmonds <teranceedmonds20@gmail.com>
2024-12-06 16:13:14 +0530

    File path fix

--- kong/example/install.sh
+++ kong/example/install.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
 # Source the configuration file
-source ./config.sh "$@" 
+source ../config.sh "$@" 
 
 echo $'\n# Install echo service #'
 kubectl apply -f https://docs.konghq.com/assets/kubernetes-ingress-controller/examples/echo-service.yaml -n $namespace
 
 echo $'\n# Install httproute #'
-kubectl apply -f ./example/httproute.yaml -n $namespace
+kubectl apply -f ./httproute.yaml -n $namespace
 
 echo $'\n=== Completed ==='
