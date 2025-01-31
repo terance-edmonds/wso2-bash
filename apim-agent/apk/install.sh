53cc218d4cbe5823fb16ba87a86fa4047225295e
Terance Edmonds <teranceedmonds20@gmail.com>
2025-01-31 20:49:44 +0530

    Kong gateway with apim deployment

--- apim-agent/apk/install.sh
+++ apim-agent/apk/install.sh
@@ -4,19 +4,25 @@ source ../../loader.sh
 trap BLA::stop_loading_animation SIGINT
 
 # Source the configuration file
-source ../config.sh "$@" 
+namespace=apk
+helm_apim_name=apim
+helm_apk_name=apk
+helm_agent_name=apim-apk-agent
 
-echo $'### Installing Agent With Control Plane (Portal Driven) ###'
+echo $'### Installing APK With Control Plane (Portal Driven) ###'
 echo $'namespace: '$namespace$''
 echo $'ingress namespace: ingress-nginx'
 echo $'helm APIM name: '$helm_apim_name$''
-echo $'helm APIM Agent name: '$helm_agent_name$''
+echo $'helm APK name: '$helm_apk_name$''
+echo $'helm APIM-APK Agent name: '$helm_agent_name$''
 
 echo $'\n# Create namespace #'
 kubectl create ns $namespace
 
-echo $'\n# Add APIM CP, DP APIM-APK Agent repos #'
+echo $'\n# Add APIM CP, APK DP, APIM-APK Agent repos #'
 helm repo add wso2apim https://github.com/wso2/helm-apim/releases/download/cp-4.4.0
+helm repo add wso2apk https://github.com/wso2/apk/releases/download/1.2.0
+helm repo add wso2apkagent https://github.com/wso2/product-apim-tooling/releases/download/1.2.0
 helm repo update
 
 echo $'\n# Install APIM CP #'
@@ -29,7 +35,13 @@ BLA::start_loading_animation "${BLA_braille_whitespace[@]}"
 helm upgrade --install ingress-nginx ingress-nginx --repo https://kubernetes.github.io/ingress-nginx --namespace ingress-nginx --create-namespace
 BLA::stop_loading_animation
 
-echo $'\n# Install APIM Agent #'
+echo $'\n# Install APK DP #'
+BLA::start_loading_animation "${BLA_braille_whitespace[@]}"
+helm install $helm_apk_name wso2apk/apk-helm --version 1.2.0 -f https://raw.githubusercontent.com/wso2/apk/main/helm-charts/samples/apk/1.2.0-cp-enabled-values.yaml -n $namespace
+BLA::stop_loading_animation
+
+
+echo $'\n# Install APIM-APK Agent #'
 BLA::start_loading_animation "${BLA_braille_whitespace[@]}"
 helm install $helm_agent_name ./helm/. -n $namespace
 BLA::stop_loading_animation
